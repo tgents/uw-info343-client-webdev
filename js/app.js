05bbcf90423481d7b13e024de3b6e28530d54d0e
Tom <tgents@gmail.com>
2014-11-09 16:09:08 -0800

    enable functions using angular

--- js/app.js
+++ js/app.js
@@ -5,3 +5,15 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+angular.module('AddressBook', [])
+	.controller('AddressController', function($scope){
+		$scope.employees = pawneeEmployees;
+		$scope.sortCol = 'lastName';
+		$scope.searchString = '';
+
+		$scope.sortBy = function(colName){
+			$scope.sortReverse = $scope.sortCol == colName ? !$scope.sortReverse : false; 
+			$scope.sortCol = colName;
+		}
+	});
